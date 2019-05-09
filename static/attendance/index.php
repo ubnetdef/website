9b1b4dfff52e16afd6bb013326da94952b755e19
vtumwa <31749089+vtumwa@users.noreply.github.com>
2019-05-09 08:05:13 -0400

    Update

--- static/attendance/index.php
+++ static/attendance/index.php
@@ -1,4 +1,4 @@
 <?php
-$url = 'https://docs.google.com/forms/d/e/1FAIpQLSeByv-yQ_1O5GNqiIK6dTJlpKV25zM5rO4y5YtMLiJUaBuWlA/viewform?usp=sf_link';
+$url = 'https://docs.google.com/forms/d/e/1FAIpQLSe8apzsN9PEYk5_Ly1yeeiTNNWKKU3yUlGkFOnhcrUspYpKEw/viewform?usp=sf_link';
 
 header('Location: ' . $url);
