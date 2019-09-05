b0a9b77c4c9046d9bd5dfdd72124e8655f41477e
mavrogorgeous <53914973+mavrogorgeous@users.noreply.github.com>
2019-09-05 15:51:24 -0400

    Update index.php (#6)
    Updated attendance for 9/5/2019

--- static/attendance/index.php
+++ static/attendance/index.php
@@ -1,4 +1,4 @@
 <?php
-$url = 'https://docs.google.com/forms/d/e/1FAIpQLSe50cdCFH4qKR3smTMiDZB0no0zYWBZgQrVSk5dUD1XPMcPlw/viewform?usp=sf_link';
+$url = 'https://forms.gle/QPg8rmbA4KoGbm6u9';
 
 header('Location: ' . $url);
