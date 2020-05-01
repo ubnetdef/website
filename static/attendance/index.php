6ca6d194227a5561c1104f11dfe6df7b2817769c
Aritra-Paul <aritrapaul48.ap@gmail.com>
2020-04-30 20:02:53 -0400

    04/30/2020

--- static/attendance/index.php
+++ static/attendance/index.php
@@ -1,4 +1,4 @@
 <?php
-$url = 'https://docs.google.com/forms/d/e/1FAIpQLSclat7NVzInWaO9PShQXx8SS0EQhl22cpfBAdr1cl8Fbm5Mnw/viewform?usp=pp_url';
+$url = 'https://docs.google.com/forms/d/e/1FAIpQLSelUJ7sF2uN4ndCje6wSIuDoX-zA8z14v39iL2RrjGN46Sqew/viewform?usp=pp_url';
 
 header('Location: ' . $url);
