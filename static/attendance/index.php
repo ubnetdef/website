1a37bd7bc1f7f28c1e8c78da6a13cc5b3239fa46
Aritra Paul <ari@Aritras-MacBook-Pro.local>
2020-09-24 18:45:01 -0400

    http link updated

--- static/attendance/index.php
+++ static/attendance/index.php
@@ -1,4 +1,4 @@
 <?php
-$url = 'hhttps://docs.google.com/forms/d/e/1FAIpQLSeU88xDoxN1G4EFJ938TdzZWM6NhVaHH-wYOLxzcIRYnkNqqA/viewform?usp=pp_url';
+$url = 'https://docs.google.com/forms/d/e/1FAIpQLSeU88xDoxN1G4EFJ938TdzZWM6NhVaHH-wYOLxzcIRYnkNqqA/viewform?usp=pp_url';
 
 header('Location: ' . $url);
