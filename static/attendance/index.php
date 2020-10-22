bfc9e02f1124ec37964fce7552e98c5d14970fca
Aritra Paul <ari@aritras-mbp.myfiosgateway.com>
2020-10-22 19:03:13 -0400

    10/22/2020 Attendance

--- static/attendance/index.php
+++ static/attendance/index.php
@@ -1,4 +1,4 @@
 <?php
-$url = 'https://docs.google.com/forms/d/e/1FAIpQLSdC6hfsIEOLdI0o4huTqKece6uomld8_t6fn4q1hCSmqZMnlw/viewform?usp=pp_url';
+$url = 'https://docs.google.com/forms/d/e/1FAIpQLSe7DiuPdhZm_F2X9N4g2nXelTEelepRjr4W36oezEQrt4yOtA/viewform?usp=pp_url';
 
 header('Location: ' . $url);
