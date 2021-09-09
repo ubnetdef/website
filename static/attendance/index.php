61b8817164aed6b9a6ea3228423814070fed3690
antjp <47665110+antjp@users.noreply.github.com>
2021-09-09 13:39:53 -0400

    Attendance Week 2

--- static/attendance/index.php
+++ static/attendance/index.php
@@ -1,4 +1,4 @@
 <?php
-$url = 'https://docs.google.com/forms/d/e/1FAIpQLSfXx_NiSGjYOPz3ipljUOyYey78eKwXfSJqQFLe7zRF5sBu7w/viewform?usp=pp_url';
+$url = 'https://docs.google.com/forms/d/e/1FAIpQLSd9kPkBHUUp3rI3QSkyKeoMvTF1IdBDY7R_dxjxEyUFTsiTAg/viewform?usp=sf_link';
 
 header('Location: ' . $url);
