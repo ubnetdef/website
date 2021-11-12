6afba44af969aa4de62519c0007fb115048c1aa0
antjp <47665110+antjp@users.noreply.github.com>
2021-11-11 19:32:17 -0500

    Update index.php

--- static/attendance/index.php
+++ static/attendance/index.php
@@ -1,4 +1,4 @@
 <?php
-$url = 'https://docs.google.com/forms/d/e/1FAIpQLSeRQv7B-5EhdrL4VyrRtCFTwFJJz6qUfEt51Mns1TujoN1qQg/viewform?usp=sf_link';
+$url = 'https://docs.google.com/forms/d/e/1FAIpQLSfXRJZ05Gz8QDEJsmlDPXIuagQMRq1RM2fmCoto2lK6LUcmUA/viewform?usp=sf_link';
 
 header('Location: ' . $url);
