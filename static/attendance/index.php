3f07aecc241a3abd6810798a29396b70a58fda31
Alex Skowronski <76066748+adskowro@users.noreply.github.com>
2022-04-26 19:45:08 -0400

    Update index.php

--- static/attendance/index.php
+++ static/attendance/index.php
@@ -1,4 +1,4 @@
 <?php
-$url = 'https://docs.google.com/forms/d/e/1FAIpQLSe7W0jTKVEgIYJOSZknWNYYXU3dCeMVxvyk9zCCVME0UJd4hQ/viewform?usp=sf_link';
+$url = 'https://docs.google.com/forms/d/e/1FAIpQLScw_yzGJsvL4DpzSWr4ODguZJlf06Y7kXczW4wc_g_pV9JeHA/viewform?usp=sf_link';
 
 header('Location: ' . $url);
